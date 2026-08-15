4b8172c495af1bdd6e0369294225a0d1ac59bd33
Matéo H. Petel <113530345+fraware@users.noreply.github.com>
2026-08-15 03:54:39 -0700

    Add realtime instrument worklet

---
+++ apps/web/src/instrument-processor.ts
@@ -0,0 +1,54 @@
+import {
+  ModalInstrumentEngine,
+  type ModalInstrumentWorkletMessage,
+} from "@everything-rings/instrument";
+
+declare const sampleRate: number;
+declare abstract class AudioWorkletProcessor {
+  readonly port: MessagePort;
+  abstract process(
+    inputs: Float32Array[][],
+    outputs: Float32Array[][],
+    parameters: Record<string, Float32Array>,
+  ): boolean;
+}
+declare function registerProcessor(
+  name: string,
+  processorCtor: new (options?: AudioWorkletNodeOptions) => AudioWorkletProcessor,
+): void;
+
+const PROCESSOR_NAME = "everything-rings-instrument";
+
+class EverythingRingsInstrumentProcessor extends AudioWorkletProcessor {
+  private engine: ModalInstrumentEngine | undefined;
+
+  constructor() {
+    super();
+    this.port.onmessage = (event: MessageEvent<ModalInstrumentWorkletMessage>) => {
+      const message = event.data;
+      if (message.type === "SET_FINGERPRINT") {
+        if (this.engine === undefined) {
+          this.engine = new ModalInstrumentEngine(sampleRate, message.fingerprint);
+        } else {
+          this.engine.setFingerprint(message.fingerprint);
+        }
+        return;
+      }
+      if (message.type === "NOTE_ON") {
+        this.engine?.noteOn(message.midiNote, message.velocity);
+        return;
+      }
+      this.engine?.allNotesOff();
+    };
+  }
+
+  process(_inputs: Float32Array[][], outputs: Float32Array[][]): boolean {
+    const output = outputs[0]?.[0];
+    if (output === undefined) return true;
+    if (this.engine === undefined) output.fill(0);
+    else this.engine.process(output);
+    return true;
+  }
+}
+
+registerProcessor(PROCESSOR_NAME, EverythingRingsInstrumentProcessor);
