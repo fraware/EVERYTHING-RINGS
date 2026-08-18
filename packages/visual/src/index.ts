a04b30efddc64eb2eaa6969fcc63b2a4a60841bc
Matéo H. Petel <113530345+fraware@users.noreply.github.com>
2026-08-18 01:48:46 -0700

    Export Acoustic Story generator

--- packages/visual/src/index.ts
+++ packages/visual/src/index.ts
@@ -1,4 +1,5 @@
 export { createAcousticCardSvg } from "./acoustic-card";
+export { createAcousticStoryHtml } from "./acoustic-story";
 export {
   DEFAULT_ACOUSTIC_DNA_CONFIG,
   acousticDnaSourceModeIndices,
