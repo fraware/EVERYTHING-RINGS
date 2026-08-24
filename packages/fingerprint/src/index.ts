3416b33138503632f681f694ce85c6f8989f47f8
Matéo H. Petel <113530345+fraware@users.noreply.github.com>
2026-08-24 08:11:50 -0700

    Export Sonic Twin baseline API

--- packages/fingerprint/src/index.ts
+++ packages/fingerprint/src/index.ts
@@ -6,3 +6,13 @@ export {
   type ModeMatch,
   type ModeRecurrenceConfig,
 } from "./recurrence";
+export {
+  DEFAULT_SONIC_TWIN_BASELINE_CONFIG,
+  benchmarkSonicTwinBaseline,
+  compareSonicTwinBaseline,
+  type LabeledTwinPair,
+  type SonicTwinBaselineComparison,
+  type SonicTwinBaselineConfig,
+  type SonicTwinBenchmarkMetrics,
+  type SonicTwinDecision,
+} from "./sonic-twin";
